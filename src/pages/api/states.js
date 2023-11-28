070592a7364f20731e3b915d2e9d08ddd6d023f0
Amir Javaid <140701314+sameerzahidk@users.noreply.github.com>
2023-11-28 12:34:30 +0500

    states_update

--- src/pages/api/states.js
+++ src/pages/api/states.js
@@ -6,29 +6,29 @@ export default async (req, res) => {
     const collection = db.collection('destinations'); // Replace with your collection name
 
   try {
-    const headersJson = JSON.stringify(req.headers)
-    const query = req.query;
-    let response = await fetch("https://api.test.hotelbeds.com/hotel-content-api/1.0/locations/destinations?fields=all&countryCodes=AU&countryCodes=US&countryCodes=AE&countryCodes=ID&countryCodes=SG&countryCodes=UK&countryCodes=IN&countryCodes=TH&countryCodes=MY&countryCodes=HK&countryCodes=LK&countryCodes=JP&from=2000&to=2999", {
-      method: 'GET',
+    // const headersJson = JSON.stringify(req.headers)
+    // const query = req.query;
+    // let response = await fetch("https://api.test.hotelbeds.com/hotel-content-api/1.0/locations/destinations?fields=all&countryCodes=AU&countryCodes=US&countryCodes=AE&countryCodes=ID&countryCodes=SG&countryCodes=UK&countryCodes=IN&countryCodes=TH&countryCodes=MY&countryCodes=HK&countryCodes=LK&countryCodes=JP&from=2000&to=2999", {
+    //   method: 'GET',
 
-      headers: {
-        'Api-key': process.env.apikey,
-        'X-Signature': req.headers["x-signature"], //it can be iPhone or your any other attribute
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Origin': '*',
-        'X-Requested-With': '*',
-        'Accept': 'application/json',
-        'Accept-Encoding': 'g-zip',
-        'Accept': '*'
-      },
-    })
-    let data = await response.json();
-      //const result = await collection.insertMany(data.destinations);
+    //   headers: {
+    //     'Api-key': process.env.apikey,
+    //     'X-Signature': req.headers["x-signature"], //it can be iPhone or your any other attribute
+    //     'Content-Type': 'application/json',
+    //     'Access-Control-Allow-Methods': '*',
+    //     'Access-Control-Allow-Credentials': true,
+    //     'Access-Control-Allow-Origin': '*',
+    //     'X-Requested-With': '*',
+    //     'Accept': 'application/json',
+    //     'Accept-Encoding': 'g-zip',
+    //     'Accept': '*'
+    //   },
+    // })
+    // let data = await response.json();
+    //   const result = await collection.insertMany(data.destinations);
     // console.log(data,result, "result in db")
  
-    const states = await collection.insertMany(data.destinations, { ordered: false });
+    // const states = await collection.insertMany(data.destinations, { ordered: false });
 
      const documents = await collection.find({}).toArray();
     if (documents) {
