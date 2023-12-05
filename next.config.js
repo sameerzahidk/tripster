30a0fd65d6ccc15810a5398f7b63a80fa2888a7e
Amir Javaid <140701314+sameerzahidk@users.noreply.github.com>
2023-12-05 17:19:35 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ module.exports = {
   // nextConfig,
   env: {
     HOTELBEDS_BASE_URL: "https://api.test.hotelbeds.com",
-    NEXT_URL: process.env.NODE_ENV == 'development' ? "http://localhost:3008" : "https://tripster-seven.vercel.app/",
+    NEXT_URL: process.env.NODE_ENV == 'development' ? "http://localhost:3008" : "https://tripster-beta.vercel.app/",
     BASE_URL: 'https://api.test.hotelbeds.com/',
     // http://tripster.technorthstar.com
-    NEXTAUTH_URL: process.env.NODE_ENV == 'development' ? "http://localhost:3008" : "https://tripster-seven.vercel.app/",
+    NEXTAUTH_URL: process.env.NODE_ENV == 'development' ? "http://localhost:3008" : "https://tripster-beta.vercel.app/",
     // Farooq
     //apikey: '49295928a2cfa7b4b8c2da59d2fe6413',
     //apiSecret: '91f9041c26',
@@ -25,7 +25,7 @@ module.exports = {
     apikey: '114443926226a3133a5a8b3207598b15',
     apiSecret: 'f56518c168',
     signature: 'b87b904dd8d22586a7063c4cd68a6f0ab2b5a1ccf50d1d0238e084ab3f54f3e8',
-    LOCAL_AUTH_URL: process.env.NODE_ENV == 'development' ? "http://localhost:3008" : "https://tripster-seven.vercel.app/",
+    LOCAL_AUTH_URL: process.env.NODE_ENV == 'development' ? "http://localhost:3008" : "https://tripster-beta.vercel.app/",
     // mongodb://tripster:tripSter@64.227.177.4:27017/?authMechanism=DEFAULT
     MONGODB_URI: process.env.NODE_ENV == 'development' ? 'mongodb+srv://tripster:Kuchnahi021!@cluster0.gkfwuva.mongodb.net/' : 'mongodb+srv://tripster:Kuchnahi021!@cluster0.gkfwuva.mongodb.net/',
     GOOGLE_CLIENT_ID: '1029873607722-n24phnvjrhkj69afndg7kq947l9eumt4.apps.googleusercontent.com',
